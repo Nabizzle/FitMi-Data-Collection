767b324286749dbd57da924bc8e27c1e749626e6
Nabeel <16357000+Nabizzle@users.noreply.github.com>
2022-12-27 21:45:47 -0500

    fix: Addresses spelling in PuckTask script

--- FitMi_Python3_Converstion/Puck/puckTask.py
+++ FitMi_Python3_Converstion/Puck/puckTask.py
@@ -4,16 +4,16 @@
 ## HID puck version of the puck task class - try to make it similar.
 
 class PuckTask(object):
-    DOFS = {'r':0, 'p': 1, 'y': 2}
+    degrees_of_freedom = {'r':0, 'p': 1, 'y': 2}
     def __init__(self, dof='p'):
-        self.dof = self.DOFS[dof]  # we actually want the index
+        self.dof = self.degrees_of_freedom[dof]  # we actually want the index
         self.state = 0
         self.pos_reference = 0
         self.target = 15
 
-    def checkStateATrigger(self, rehabtouch):
+    def checkStateATrigger(self, rehab_touch):
         if self.state == 1: return
-        pos = rehabtouch.puckpack1.rpy[0,self.dof]
+        pos = rehab_touch.puck_packet_1.rpy[0,self.dof]
         if not pos: return
         if (pos - self.pos_reference) < -self.target:
             self.state = 1
@@ -21,9 +21,9 @@ class PuckTask(object):
 
         return False
 
-    def checkStateBTrigger(self, rehabtouch):
+    def checkStateBTrigger(self, rehab_touch):
         if self.state == 0: return
-        pos = rehabtouch.puckpack1.rpy[0,self.dof]
+        pos = rehab_touch.puck_packet_1.rpy[0,self.dof]
         if not pos: return
         if (pos - self.pos_reference) > self.target:
             self.state = 0
