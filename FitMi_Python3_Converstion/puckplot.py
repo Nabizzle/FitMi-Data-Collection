2e1d40fde0577095ee6ae050a68b1eb9de4075f9
Nabeel <16357000+Nabizzle@users.noreply.github.com>
2022-12-27 22:20:25 -0500

    fix: Corrects warnings in puckplot script

--- FitMi_Python3_Converstion/puckplot.py
+++ FitMi_Python3_Converstion/puckplot.py
@@ -14,48 +14,48 @@ class PuckPlotter(object):
         self.fs = 60
         self.n_seconds = 100
         self.max_samples = self.fs*self.n_seconds
-        buffmin = 0
-        buffmax = 200
+        buffer_min = 0
+        buffer_max = 200
 
         ymax = 180
         ymin = -180
 
         self.fig = plt.figure()
         self.fig.set_size_inches(6, 10, forward=True)
-        self.plt1 = AniPlot(self.fig, 411, buffmin, buffmax,
+        self.plot_1 = AniPlot(self.fig, 411, buffer_min, buffer_max,
                                             ymin, ymax, double=True)
 
-        self.plt2 = AniPlot(self.fig, 412, buffmin, buffmax,
+        self.plot_2 = AniPlot(self.fig, 412, buffer_min, buffer_max,
                                             -500, 500, double=True)
 
-        self.plt3 = AniPlot(self.fig, 413, buffmin, buffmax,
+        self.plot_3 = AniPlot(self.fig, 413, buffer_min, buffer_max,
                                             -1000, 1000, double=True)
 
-        self.lcplot = AniPlot(self.fig, 414, buffmin, buffmax,
+        self.lc_plot = AniPlot(self.fig, 414, buffer_min, buffer_max,
                                         ymin=-100, ymax=1100, double=True)
-        self.lcplot_red = False
+        self.lc_plot_red = False
         self.time = 0
         self.puck = HIDPuckDongle()
-        self.plt1.set_ylabel("vert angle")
-        self.plt2.set_ylabel("gyro x")
-        self.plt3.set_ylabel("accel x")
-        self.lcplot.set_ylabel("loadcell")
+        self.plot_1.set_ylabel("vert angle")
+        self.plot_2.set_ylabel("gyro x")
+        self.plot_3.set_ylabel("accel x")
+        self.lc_plot.set_ylabel("load cell")
 
     def start(self):
         self.puck.open()
         self.puck.sendCommand(0,SENDVEL, 0x00, 0x01)
         self.puck.sendCommand(1,SENDVEL, 0x00, 0x01)
         #self.puck.setTouchBuzz(1,0)
-        tickup = 0
+        tick_up = 0
         for i in range(0, 10000):
             self.puck.checkForNewPuckData()
             self.run(self.puck.puck_packet_0, self.puck.puck_packet_1)
-            # print "connected ", self.puck.puckpack0.connected, self.puck.puckpack1.connected
+            # print "connected ", self.puck.puck_packet_0.connected, self.puck.puck_packet_1.connected
             #print "pipes ", self.puck.block0_pipe, self.puck.block1_pipe
             time.sleep(1.0/self.fs)
-            tickup+=1
-            if tickup > self.fs:
-                tickup=0
+            tick_up+=1
+            if tick_up > self.fs:
+                tick_up=0
                 #print".",
 
     def stop(self):
@@ -64,37 +64,37 @@ class PuckPlotter(object):
         #self.puck.setTouchBuzz(1,1)
         self.puck.close()
 
-    def run(self, puckdata=None, puckdata2=None):
+    def run(self, puck_data=None, puck_data2=None):
         # update the xy data
-        self.update_buffers(puckdata, puckdata2)
+        self.update_buffers(puck_data, puck_data2)
 
-        self.plt1.draw(self.fig)
-        self.plt2.draw(self.fig)
-        self.plt3.draw(self.fig)
-        self.lcplot.draw(self.fig)
+        self.plot_1.draw(self.fig)
+        self.plot_2.draw(self.fig)
+        self.plot_3.draw(self.fig)
+        self.lc_plot.draw(self.fig)
 
-        if puckdata.touch:
-            self.lcplot.plt.set_color("r")
+        if puck_data.touch:
+            self.lc_plot.plt.set_color("r")
         else:
-            self.lcplot.plt.set_color("b")
+            self.lc_plot.plt.set_color("b")
 
-        if puckdata2.touch and self.lcplot.plt2:
-            self.lcplot.plt2.set_color("r")
+        if puck_data2.touch and self.lc_plot.plt2:
+            self.lc_plot.plt2.set_color("r")
         else:
-            self.lcplot.plt2.set_color("b")
+            self.lc_plot.plt2.set_color("b")
 
     ##---- the real update function ------------------------------------------##
-    def update_buffers(self, puckdata, puckdata2):
-        self.plt1.update(puckdata.getVertAngle(), puckdata2.getVertAngle())
-        self.plt2.update(puckdata.gyro[0,0], puckdata2.gyro[0,0])
-        self.plt3.update(puckdata.accel[0,0], puckdata2.accel[0,0])
-        self.lcplot.update(puckdata.loadcell, puckdata2.loadcell)
-        puckdata.getVertAngle()
+    def update_buffers(self, puck_data, puck_data2):
+        self.plot_1.update(puck_data.getVertAngle(), puck_data2.getVertAngle())
+        self.plot_2.update(puck_data.gyroscope[0,0], puck_data2.gyroscope[0,0])
+        self.plot_3.update(puck_data.accelerometer[0,0], puck_data2.accelerometer[0,0])
+        self.lc_plot.update(puck_data.load_cell, puck_data2.load_cell)
+        puck_data.getVertAngle()
 
-        if np.linalg.norm(puckdata2.accel) > 1500:
+        if np.linalg.norm(puck_data2.accel) > 1500:
             self.puck.actuate(1, 500, 100)
         #self.puck.sendCommand(0, RBLINK, 0x01, 0x21)
-        #print puckdata.resv3 # resv3 is currently set up to tell us if in gaming state
+        #print(puck_data.res_v5) # res_v5 is currently set up to tell us if in gaming state
 
 if __name__ == '__main__':
     plotter = PuckPlotter()
