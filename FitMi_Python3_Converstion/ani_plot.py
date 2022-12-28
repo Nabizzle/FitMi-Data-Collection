2832612cb07f94d82201105f01b089faedd41c56
Nabeel <16357000+Nabizzle@users.noreply.github.com>
2022-12-27 21:58:25 -0500

    fix: spelling of variables clearer in ani_plot

--- FitMi_Python3_Converstion/ani_plot.py
+++ FitMi_Python3_Converstion/ani_plot.py
@@ -5,9 +5,9 @@
 from matplotlib import pyplot as plt
 
 class AniPlot(object):
-    def __init__(self, fig, spltnum, buffmin=0, buffmax=200, ymin=-180, ymax=180, double=False):
-        self.ax = fig.add_subplot(spltnum)
-        self.ax.set_xlim(buffmin, buffmax)
+    def __init__(self, fig, spilt_number, buffer_min=0, buffer_max=200, ymin=-180, ymax=180, double=False):
+        self.ax = fig.add_subplot(spilt_number)
+        self.ax.set_xlim(buffer_min, buffer_max)
         self.ax.set_ylim(ymin, ymax)
         self.ax.hold(True)
 
@@ -16,22 +16,22 @@ class AniPlot(object):
 
         self.bg = fig.canvas.copy_from_bbox(self.ax.bbox)
 
-        self.buff = [0]*(buffmax - buffmin)
+        self.buff = [0]*(buffer_max - buffer_min)
         self.buff2 = None
         if double:
-            self.buff2 = [0]*(buffmax - buffmin)
-        self.xpts = range(buffmin, buffmax)
+            self.buff2 = [0]*(buffer_max - buffer_min)
+        self.x_points = range(buffer_min, buffer_max)
 
-        self.plt = self.ax.plot(self.xpts, self.buff, '-', color="b")[0]
+        self.plt = self.ax.plot(self.x_points, self.buff, '-', color="b")[0]
         self.plt2 = None
         if double:
-            self.plt2 = self.ax.plot(self.xpts, self.buff2, '-', color="g")[0]
+            self.plt2 = self.ax.plot(self.x_points, self.buff2, '-', color="g")[0]
 
-    def set_xlabel(self, axname):
-        self.ax.set_xlabel(axname)
+    def set_xlabel(self, axis_name):
+        self.ax.set_xlabel(axis_name)
 
-    def set_ylabel(self, axname):
-        self.ax.set_ylabel(axname)
+    def set_ylabel(self, axis_name):
+        self.ax.set_ylabel(axis_name)
 
     def update(self, data1, data2=None):
         self.buff.pop(0)
@@ -42,9 +42,9 @@ class AniPlot(object):
             self.buff2.append(data2)
 
     def draw(self, fig):
-        self.plt.set_data(self.xpts, self.buff)
+        self.plt.set_data(self.x_points, self.buff)
         if self.plt2 and self.buff2:
-            self.plt2.set_data(self.xpts, self.buff2)
+            self.plt2.set_data(self.x_points, self.buff2)
 
         fig.canvas.restore_region(self.bg)
         self.ax.draw_artist(self.plt)
