55953f7f02ee7a6e7262f9ff7dd7e1124e0ffc76
Nabeel <16357000+Nabizzle@users.noreply.github.com>
2022-12-29 22:53:20 -0500

    docs: Adds documentation to plot_log_file script

--- FitMi_Python3_Converstion/plot_log_file.py
+++ FitMi_Python3_Converstion/plot_log_file.py
@@ -1,26 +1,52 @@
-##----------------------------------------------------------------------------##
-##---- plot logged data ------------------------------------------------------##
-##----------------------------------------------------------------------------##
+'''
+Plots data from the a log file.
 
+Takes in a Python data file and plots each of the data types to their own plot.
+
+Parameters
+----------
+data_folder : file path
+    Path to the data folder
+file_name : string
+    name of the data file
+full_path : file path
+    Path to the data file
+error_string : string
+    error message if the path to the file does not exist
+data_shelf : python data file
+    data from the log file
+fig : matplotlib.pyplot figure
+    figure for each data type
+ax : matplotlib.pyplot axis
+    axis for a data plot
+'''
 import matplotlib.pyplot as plt
-import numpy as np
 import os
 import shelve
 
+# get the path to the data folder
 data_folder = os.path.join(os.getcwd(), "data")
-fname = input("enter the name of the file you want to plot (without extension): ")
-fname += ".shelve"
-fullpath = os.path.join(data_folder, fname)
-print(fullpath)
 
-if not os.path.exists(fullpath):
-    error_string = "%s not found" % fullpath
+# ask for the data file name and add the right extension
+file_name = input("enter the name of the file you want to plot (without extension): ")
+file_name += ".shelve"
+
+# create the file path to the data file
+full_path = os.path.join(data_folder, file_name)
+print(full_path)
+
+# check if the data file path exists
+if not os.path.exists(full_path):
+    error_string = "%s not found" % full_path
     raise Exception(error_string)
 
-data_shelf = shelve.open(fullpath)
+# get the data from the log file
+data_shelf = shelve.open(full_path)
 
+# show the user the data types in the log file
 print(data_shelf.keys())
 
+# make a plot for each data type
 for key in data_shelf.keys():
     fig = plt.figure()
     ax = fig.add_subplot(111)
@@ -28,6 +54,6 @@ for key in data_shelf.keys():
     ax.set_title(key)
     fig.show()
 
-data_shelf.close()
+data_shelf.close() # close the data file
 
-input("press enter to close all")
+input("press enter to close all") # wait for user to end the script
