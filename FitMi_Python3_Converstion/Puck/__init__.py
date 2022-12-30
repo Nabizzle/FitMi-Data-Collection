5adb4651b326525e82cf9b38726197a35c1591b4
Nabeel <16357000+Nabizzle@users.noreply.github.com>
2022-12-30 18:53:06 -0500

    docs: Documents the Puck library scripts

--- FitMi_Python3_Converstion/Puck/__init__.py
+++ FitMi_Python3_Converstion/Puck/__init__.py
@@ -9,5 +9,10 @@ hid_puck
     The definition for the HIDPuckDongle class. It controls all of the communication to and from the dongle for the FitMi pucks
 puck_packet
     The definition for the PuckPacket class. It parses and contains the data from the pucks for the accelerometer, gyroscope, magnetometer, velocity measurement, quaternion, roll, pitch, and yaw angles; load cell, battery, charging indicator, connection indicator, touch status, status for the IMU, status for if the velocity is polled, the state, and a variable called res_v5
-puck
+puck_task
+    Class definition of PuckTask. Analyzes a single degree of freedom and changes a state variable when the degree of freedom of the puck moves above and below a specific angle range.
+quaternion
+    Helper functions for working with quaternions
+scan_packet
+    The definition of the ScanPacket class. It characterizes the pipe channels of the scan.
 '''
