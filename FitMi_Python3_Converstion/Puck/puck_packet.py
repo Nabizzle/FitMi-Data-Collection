fix: Squashes bug where quaternions were zero
The numpy array of the quaternions had to be made floats from the
beginning

FitMi_Python3_Converstion/Puck/puck_packet.py
@@ -75,7 +75,7 @@ class PuckPacket(object):
         self.gyroscope = np.array([0,0,0])
         self.magnetometer = np.array([0,0,0])
         self.velocity = np.array([0,0,0]) # only updates if activated when puck connected to.
-        self.quaternion = np.array([0,0,0,0])
+        self.quaternion = np.array([0.0, 0.0, 0.0, 0.0])
         self.roll_pitch_yaw = np.array([0,0,0])
         self.load_cell = 0
         self.battery = 0