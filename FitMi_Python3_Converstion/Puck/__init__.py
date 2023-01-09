cbcad1416bf79eae9f9976262cadadbeb8873bf7
Nabeel <16357000+Nabizzle@users.noreply.github.com>
2023-01-09 12:43:17 -0500

    style: Updates Puck __init__.py to PEP8

--- FitMi_Python3_Converstion/Puck/__init__.py
+++ FitMi_Python3_Converstion/Puck/__init__.py
@@ -1,18 +1,31 @@
 '''
-A library to connect to each of the FitMi pucks, poll their data and process it.
+A library to connect each of the FitMi pucks, poll their data and process it.
 
-This library has code to connect the dongle for the FitMi pucks and poll data from their accelerometers, gyroscope, magnetometer, and load cell. Data also available is the linear velocity and quaternion representing the rotation of each puck, the battery, and status of the pucks among other variables and helper functions for analyzing the data.
+This library has code to connect the dongle for the FitMi pucks and poll data
+from their accelerometers, gyroscope, magnetometer, and load cell. Data also
+available is the linear velocity and quaternion representing the rotation of
+each puck, the battery, and status of the pucks among other variables and
+helper functions for analyzing the data.
 
 Modules
 -------
 hid_puck
-    The definition for the HIDPuckDongle class. It controls all of the communication to and from the dongle for the FitMi pucks
+    The definition for the HIDPuckDongle class. It controls all of the
+    communication to and from the dongle for the FitMi pucks
 puck_packet
-    The definition for the PuckPacket class. It parses and contains the data from the pucks for the accelerometer, gyroscope, magnetometer, velocity measurement, quaternion, roll, pitch, and yaw angles; load cell, battery, charging indicator, connection indicator, touch status, status for the IMU, status for if the velocity is polled, the state, and a variable called res_v5
+    The definition for the PuckPacket class. It parses and contains the data
+    from the pucks for the accelerometer, gyroscope, magnetometer, velocity
+    measurement, quaternion, roll, pitch, and yaw angles; load cell, battery,
+    charging indicator, connection indicator, touch status, status for the IMU,
+    status for if the velocity is polled, the state, and a variable called
+    res_v5
 puck_task
-    Class definition of PuckTask. Analyzes a single degree of freedom and changes a state variable when the degree of freedom of the puck moves above and below a specific angle range.
+    Class definition of PuckTask. Analyzes a single degree of freedom and
+    changes a state variable when the degree of freedom of the puck moves above
+    and below a specific angle range.
 quaternion
     Helper functions for working with quaternions
 scan_packet
-    The definition of the ScanPacket class. It characterizes the pipe channels of the scan.
+    The definition of the ScanPacket class. It characterizes the pipe channels
+    of the scan.
 '''
